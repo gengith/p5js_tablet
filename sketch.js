0524ceabe00d9268da801475e2b13480c92dd41a
gengith <79147708+gengith@users.noreply.github.com>
2021-03-06 06:14:47 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 // TouchedTexts
-let version = "2.9";
+let version = "2.91";
 let main;
 let myimages = [];
 let touchinput = {
@@ -21,7 +21,7 @@ class Main {
 	}
 
 	draw() {
-		background('rgb(0,100,150)');
+		//background('rgb(0,100,150)');
 		if (this.sequence === 1){  //メニュー画面
 			this.menu.proc();
 			this.menu.draw();
@@ -48,10 +48,8 @@ class Menu {
 		this.selectmenu = [  // メニュー文字、メニュー位置ｘ、メニュー位置y、メニュー色、メニュー文字サイズ、選択判定bool、選択判定ｘ1、選択判定y1、選択判定ｘ2、選択判定y2、セレクト先
 			['Menu',100,100,'white',50,false,0,0,0,0,0],
 			['version ' + version,250,100,'black',10,false,0,0,0,0,0],
-			['TEST1',100,200,'black',50,true,70,230,350,130,2],
-			['TEST2',100,300,'black',50,true,70,330,350,230,3],
-			['TEST3',100,400,'black',50,true,70,430,350,330,4],
-			['testa',100,500,'black',50,true,70,530,350,430,5]
+			['はじめる',100,200,'black',50,true,70,230,350,130,2],
+			['TEST',100,500,'black',50,true,70,530,350,430,5]
 		];
 		this.setMainSequenceFlag = false;
 	}
@@ -78,11 +76,9 @@ class Menu {
 				break;
 			}
 			
-			//★test
 			noFill();
 			rect(this.selectmenu[i][6],this.selectmenu[i][7],this.selectmenu[i][8] - this.selectmenu[i][6],this.selectmenu[i][9] - this.selectmenu[i][7]);
-			ellipse(this.selectmenu[i][6],this.selectmenu[i][7],10,10);
-			ellipse(this.selectmenu[i][8],this.selectmenu[i][9],10,10);
+			
 		}
 	}
 
@@ -106,42 +102,34 @@ class Menu {
 class Game {
 
 	constructor() {
-		this.tranp = [];  // id, 画像, 表示位置x, 表示位置y, 大きさx, 大きさy, 表裏flag
+		this.tranp = [];  // 0:id, 1:画像, 2:表示位置x, 3:表示位置y, 4:表裏flag
 		this.nx = 6;
 		this.ny = 5;
 		this.ty = height / this.ny;
 		this.tx = this.ty / 2;
-		
+		this.touchnum = 0;
+		this.opentranp1 = 0;
+		this.opentranp2 = 0;
 		
 		for (let i = 0; i < this.ny; i++) {
 			for (let j = 0; j < this.nx; j++) {
 				if( j + i * this.nx < 27) {
-					this.tranp.push([j + i * this.nx, myimages[j + i * this.nx], this.tx * j, this.ty * i, this.tx, this.ty, false]);
+					this.tranp.push([j + i * this.nx, myimages[j + i * this.nx], this.tx * j, this.ty * i, false]);
 				}
 			}
 		}
 		
-		shuffle(0,26); // 0番(トランプ背面用)の箇所に最後の一枚を移動させる。(左上から表示するため)
-		
+		this.shuffle(0,26); // 0番(トランプ背面用)の箇所に最後の一枚を移動させる。(左上から表示するため)
 		
 		//トランプを混ぜる操作
 		for (let i = 0;  i < 100; i++) {
-			//shuffle(random(1,26), random(1,26));
+			this.shuffle(floor(random(1,27)), floor(random(1,27)));
 		}
 		
-		//★test
-		this.tranp[5][6] = true;
-		this.tranp[7][6] = true;
-		this.tranp[9][6] = true;
 	}
 
 	shuffle(p1,p2) {
-
-this.tranp[p2][2]=0;
-
-this.tranp[p2][3]=0;
-
-/*
+		
 		let tmp_x = this.tranp[p1][2];
 		let tmp_y = this.tranp[p1][3];
 		
@@ -149,23 +137,59 @@ this.tranp[p2][3]=0;
 		this.tranp[p1][3] = this.tranp[p2][3];
 		this.tranp[p2][2] = tmp_x;
 		this.tranp[p2][3] = tmp_y;
-*/
+		
 	}
 
 	proc() {
+		
+		for (let i = 1; i < 27; i++) {
+			if ( !this.tranp[i][4] && touchinput.x > this.tranp[i][2] && touchinput.x < this.tranp[i][2] + this.tx && touchinput.y > this.tranp[i][3] && touchinput.y < this.tranp[i][3] + this.ty ) {
+				
+				if ( this.touchnum === 0 ) { //1枚目オープン
+					this.tranp[i][4] = true;
+					this.touchnum++;
+					this.opentranp1 = i;
+					break;
+				}
+				
+				if ( this.touchnum === 1) { //2枚目オープン
+					this.tranp[i][4] = true;
+					this.touchnum++;
+					this.opentranp2 = i;
+					break;
+				}
+				
+			}
+		}
+		
+		if ( this.touchnum >= 2 ) {  //2枚目オープン時処理
+			
+			this.touchnum++;
+			if ( this.touchnum >= 100 ) { //しばらく待ち
+				if( this.tranp[this.opentranp1][0] % 13 === this.tranp[this.opentranp2][0] % 13 ) {
+					
+				} else {
+					this.tranp[this.opentranp1][4] = false;
+					this.tranp[this.opentranp2][4] = false;
+				}
+				
+				this.touchnum = 0;
+				
+			}
+			
+		}
 		
 	}
 
 	draw() {
 				
 		for (let i = 1; i < 27; i++) {  //tranp[0]はトランプ裏面画像のための特殊id
-			if ( !this.tranp[i][6] ) {
-				image(this.tranp[i][1],this.tranp[i][2],this.tranp[i][3],this.tranp[i][4],this.tranp[i][5]);  // トランプ表面画像表示
+			if ( this.tranp[i][4] ) {
+				image(this.tranp[i][1],this.tranp[i][2],this.tranp[i][3],this.tx,this.ty);  // トランプ表面表示
 			} else {
-				image(this.tranp[0][1],this.tranp[i][2],this.tranp[i][3],this.tranp[i][4],this.tranp[i][5]); // トランプ裏面画像表示
+				image(this.tranp[0][1],this.tranp[i][2],this.tranp[i][3],this.tx,this.ty); // トランプ裏面表示
 			}
 		}
-
 	}
 
 }
@@ -198,6 +222,5 @@ function draw(){
 function touchStarted(){
   touchinput.x = touchX
   touchinput.y = touchY
-  ellipse(touchX,touchY,10,10);
   return false;
 }
